89de079d6427d11b3eb4aaa3ac1d0ef142dfb0a7
MartinMosqueira <tinchomosqueira@gmail.com>
2022-09-13 18:14:15 -0300

    Advance in test "TestUMQuestions"

--- TestE2E-Cypress/cypress/e2e/TestUMQuestions.cy.js
+++ TestE2E-Cypress/cypress/e2e/TestUMQuestions.cy.js
@@ -1,14 +1,20 @@
 describe('empty spec', () => {
   it('passes', () => {
-    cy.visit('https://virtual.um.edu.ar/login/index.php')
+    cy.visit('https://virtual.um.edu.ar')
   })
-  it('should ', function () {
+  it('login page', function () {
     cy.get('#username').type('m.mosqueira')
     cy.get('#password').type(Cypress.env('password'))
     cy.get('#loginbtn').click()
     cy.get("#loginerrormessage").should('have.text', 'Acceso inválido. Por favor, inténtelo otra vez.')
 
-    cy.get('#usertext mr-1').click()
+    cy.get('#username').type('m.mosqueira')
+    cy.get('#password').type(Cypress.env('password'))
+    cy.get('#loginbtn').click()
+
+    cy.get('.userbutton').click()
+    cy.get('#actionmenuaction-1').click()
+    cy.get('.btn-group').find('button').and('have.class', 'btn btn-outline-secondary dropdown-toggle')
 
   });
 })
